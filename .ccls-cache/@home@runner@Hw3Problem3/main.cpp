e576b0de41e794210bf268945fb3b378b01d4166
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-11 21:10:31 +0000

    Running base format

--- .ccls-cache/@home@runner@Hw3Problem3/main.cpp
+++ .ccls-cache/@home@runner@Hw3Problem3/main.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 
 int main() {
-  \
+  float E;
+  float R;
+  float C;
+  float t;
+  float e;
+  e =  2.71828;
+  float i;
+  i=pow((E/R)*e,((-t/(R*C))));
+    
+  
+ 
+  
 }
