b1d0f536cad6956c188dcd767bf88bdee7a6037e
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-11 21:14:30 +0000

    cout assigning variables

--- main.cpp
+++ main.cpp
@@ -13,7 +13,10 @@ int main() {
   e =  2.71828;
   float i;
   i=pow((E/R)*e,((-t/(R*C))));
-    
+
+  cout<<"E is the voltage of the battery in volts. \nR is the value of the resistor in ohms.\nC is the value of the capacitor in farads. \nt is the time in seconds after the switch is closed."<<endl;
+  
+  
   
  
   
